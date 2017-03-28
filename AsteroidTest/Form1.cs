3ad9cf786581fb4868020ba74d205432a2806bb6
ChromeDev <khromenko.working@gmail.com>
2017-03-28 10:46:54 +0300

    Copy_To_Local_Rep

---
+++ AsteroidTest/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsteroidTest
+{
+    public partial class AsteroidsForm : Form
+    {
+        public static int WIDTH = 15;
+        public static int HEIGHT = 25;
+        public static int SCALE = 30;
+
+        private Timer timer;
+
+        public Graphics mainGraphics;
+        public BufferedGraphicsContext mainContext;
+        public BufferedGraphics mainBuffer;
+
+        private Ship mainShip;
+
+        private Point moveUp;
+        private Point moveDown;
+        private Point moveLeft;
+        private Point moveRight;
+
+        public AsteroidsForm()
+        {
+            InitializeComponent();
+            this.ClientSize = new Size(WIDTH * SCALE + 100, HEIGHT * SCALE);
+            pbMain.ClientSize = new Size(WIDTH * SCALE, HEIGHT * SCALE);
+            pbMain.BackColor = Color.LightGray;
+
+            mainGraphics = pbMain.CreateGraphics();
+            mainContext = new BufferedGraphicsContext();
+            mainBuffer = mainContext.Allocate(mainGraphics, new Rectangle(0, 0, pbMain.Width, pbMain.Height));
+
+            timer = new Timer();
+
+            moveUp = new Point(0, -5);
+            moveDown = new Point(0, 5);
+            moveLeft = new Point(-1, 0);
+            moveRight = new Point(1, 0);
+        }
+
+        public void RepaintForm(BufferedGraphics mainBuffer)
+        {
+            mainBuffer.Graphics.Clear(Color.LightGray);
+
+            mainShip.PaintShip(mainBuffer);
+            mainBuffer.Render();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            RepaintForm(mainBuffer);
+        }
+
+
+        private void AsteroidsForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                mainShip = new Ship(10, 10);
+                timer.Interval = 16;
+                timer.Tick += Timer_Tick;
+
+                timer.Enabled = true;
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                mainShip.Move(moveUp);
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                mainShip.Move(moveDown);
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                mainShip.Ratate(Direction.Left);
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                mainShip.Ratate(Direction.Right);
+            }
+        }
+    }
+}
---
+++ AsteroidTest/Ship.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsteroidTest
+{
+    public enum Direction{
+        Left,
+        Right,
+    }
+
+    class Ship
+    {
+        private PointF[] shipPoints;
+
+        public Ship(int startX, int startY)
+        {
+            shipPoints = new PointF[4];
+            GenerateStartPositions(startX, startY);
+        }
+
+        private void GenerateStartPositions(int startX, int startY)
+        {
+            shipPoints[0] = new Point(startX * AsteroidsForm.SCALE, startY * AsteroidsForm.SCALE);
+            shipPoints[1] = new Point((startX - 1) * AsteroidsForm.SCALE, (startY + 1) * AsteroidsForm.SCALE);
+            shipPoints[2] = new Point(startX * AsteroidsForm.SCALE, (startY - 1) * AsteroidsForm.SCALE);
+            shipPoints[3] = new Point((startX + 1) * AsteroidsForm.SCALE, (startY + 1) * AsteroidsForm.SCALE);
+        }
+
+        public void PaintShip(BufferedGraphics mainBuffer)
+        {
+            mainBuffer.Graphics.DrawPolygon(Pens.Black, shipPoints);
+        }
+
+        public void Move(Point offset)
+        {
+            for(int i = 0; i < shipPoints.Length; i++)
+            {
+                shipPoints[i].X += offset.X;
+                shipPoints[i].Y += offset.Y;
+            }
+        }
+
+        public void Ratate(Direction direct)
+        {
+            PointF[] tempshipPoints = new PointF[shipPoints.Length];
+
+            shipPoints.CopyTo(tempshipPoints, 0);
+
+            if(direct == Direction.Right)
+            {
+                for (int i = 1; i < shipPoints.Length; i++)
+                {
+                    tempshipPoints[i].X = (int)(shipPoints[0].X + (shipPoints[i].X - shipPoints[0].X) * Math.Cos(0.087) - (shipPoints[i].Y - shipPoints[0].Y) * Math.Sin(0.087));
+                    tempshipPoints[i].Y = (int)(shipPoints[0].Y + (shipPoints[i].Y - shipPoints[0].Y) * Math.Sin(0.087) + (shipPoints[i].X - shipPoints[0].X) * Math.Cos(0.087));
+
+                    //X = x0 + (x - x0) * cos(a) - (y - y0) * sin(a);
+                    //Y = y0 + (y - y0) * cos(a) + (x - x0) * sin(a);
+                }
+                shipPoints = tempshipPoints;
+            }
+
+            if (direct == Direction.Left)
+            {
+                for (int i = 1; i < shipPoints.Length; i++)
+                {
+                    tempshipPoints[i].X = (int)(shipPoints[0].X + (shipPoints[i].X - shipPoints[0].X) * Math.Cos(0.087) - (shipPoints[i].Y - shipPoints[0].Y) * Math.Sin(0.087));
+                    tempshipPoints[i].Y = (int)(shipPoints[0].Y + (shipPoints[i].Y - shipPoints[0].Y) * Math.Sin(0.087) + (shipPoints[i].X - shipPoints[0].X) * Math.Cos(0.087));
+
+
+                    //X = x0 + (x - x0) * cos(a) - (y - y0) * sin(a);
+                    //Y = y0 + (y - y0) * cos(a) + (x - x0) * sin(a);
+                }
+                shipPoints = tempshipPoints;
+            }
+        }
+
+        public void Ratate(Direction direct, BufferedGraphics mainBuffer)
+        {
+            if (direct == Direction.Right)
+            {
+                mainBuffer.Graphics.Transform.RotateAt(5, shipPoints[0]);
+            }
+
+            if (direct == Direction.Left)
+            {
+                mainBuffer.Graphics.Transform.RotateAt(-5, shipPoints[0]);
+            }
+        }
+
+    }
+}
